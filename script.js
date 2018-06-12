237aa71a69e0fa0e33e0b57353f4685abf664bc3
Septive <37156408+Septive@users.noreply.github.com>
2018-06-12 08:38:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// Variables to store the calculator screen output as strings 
+var screenOutput = "";
+
+// This function will clear all the values and set it to 0 
+function clearScreen() {
+  screenOutput = "";
+  document.getElementById("output").innerHTML = 0;
+}
+
+// This function will add the string value of the button onto the screen 
+function addValue(btnValue) {
+  
+  screenOutput += btnValue.value;
+  if(screenOutput.length > 15){
+  screenOutput = screenOutput.slice(0, 15);
+}
+  document.getElementById("output").innerHTML = screenOutput;
+    
+}
+// This function adds the operator onto the display
+function addOperator(btnValue) {
+  screenOutput += btnValue.value;
+    if(screenOutput.length > 15){
+        screenOutput = screenOutput.slice(0, 15);
+}
+  document.getElementById("output").innerHTML = screenOutput;
+}
+// This is the function that will evaluate whatever is displayed on the screen
+function equals() { 
+  screenOutput = eval(screenOutput);
+  document.getElementById("output").innerHTML = screenOutput;
+  var estring = screenOutput.toString();
+    if (estring.length > 15){
+        screenOutput = estring.slice(0, 15);
+    }
+
+}
+
+// This function changes the sign of the number to negative or positive
+function reverse(){
+    screenOutput = screenOutput * -1;
+    document.getElementById("output").innerHTML = screenOutput;
+}
+
+// This function will square root the value of the number on the screen
+function squareroot(){
+    var estring = screenOutput.toString();
+    if (estring.length > 15){
+        screenOutput = estring.slice(0, 15);
+    }
+    
+    if (screenOutput.value > 0){
+         screenOutput = Math.sqrt(screenOutput);
+         document.getElementById("output").innerHTML = screenOutput;
+    }
+    else{
+        document.getElementById("output").innerHTML = "Error";
+    }
+    
+   
+}
+
+// This function will delete the last number on the screen
+function del(){
+    screenOutput = screenOutput.substring(0, screenOutput.length - 1);
+    document.getElementById("output").innerHTML = screenOutput;
+}
+
+// This function will calculate the sin of a number
+function sin(){
+    screenOutput = Math.sin(screenOutput);
+    document.getElementById("output").innerHTML = screenOutput;
+}
+
+
+// This function will calculate the cos of a number
+function cos(){
+    screenOutput = Math.cos(screenOutput);
+    document.getElementById("output").innerHTML = screenOutput;
+}
+
+// This function will calculate the tan of a number
+function tan(){
+    screenOutput = Math.tan(screenOutput);
+    document.getElementById("output").innerHTML = screenOutput;
+}
+
+// This is my MYSTERY power function. It multiplies the value on the screen by itself
+function power(){
+    var estring = screenOutput.toString();
+    if (estring.length > 15){
+        screenOutput = estring.slice(0, 15);
+    }
+    screenOutput = screenOutput * screenOutput;
+    document.getElementById("output").innerHTML = screenOutput;
+}
